19cbe32709cadc1e24c20a073bc7051179d4ad49
sram022 <sram022@gmail.com>
2025-10-02 00:52:25 +0530

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +16,20 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    thread_data_t* thread_func_args = (thread_data_t *) thread_param;
+
+    usleep(thread_func_args ->wait_to_obtain_ms *1000);
+    pthread_mutex_lock (thread_func_args ->mutex);
+    usleep(thread_func_args ->wait_to_release_ms*1000);
+    pthread_mutex_unlock (thread_func_args ->mutex);
+
+    thread_func_args ->thread_complete_success = true;
+
+
+
+
+
     return thread_param;
 }
 
@@ -28,6 +44,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    thread_data_t *d1 = malloc(sizeof(thread_data_t));
+    *d1 = (thread_data_t) {
+    .thread_complete_success = false,
+    .mutex = mutex,
+    .wait_to_obtain_ms = wait_to_obtain_ms,
+    .wait_to_release_ms = wait_to_release_ms
+    };
+    if (pthread_create(thread, NULL, threadfunc, d1) != 0) {
+    free(d1);
     return false;
+    }
+    
+    // thread_data_t *result;
+    // pthread_join(*thread, (void**)&result);
+    
+    return  true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -7,7 +7,7 @@
  * It should be returned by your thread so it can be freed by
  * the joiner thread.
  */
-struct thread_data{
+typedef struct thread_data{
     /*
      * TODO: add other values your thread will need to manage
      * into this structure, use this structure to communicate
@@ -19,8 +19,15 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+
     bool thread_complete_success;
-};
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+
+
+
+} thread_data_t ;
 
 
 /**
